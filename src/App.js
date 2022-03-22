cb2ee5f167ebf96ce62232bdbd0f5863978b5552
gilang aditya <madegilangaditya32@gmail.com>
2022-03-22 13:54:06 +0800

    finish section 10 react variable

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 function App (){
-    return <h1>Hellow wordld from APP</h1>
+    // section 10
+    const title = 'Hellow World'
+    const body = 'This Body'
+    const comments = [
+        {id:1, text:'comment one'},
+        {id:2, text:'comment two'},
+        {id:3, text:'comment three'},
+    ]
+    // end section 10
+
+    return (
+        <div className="container">
+            {/* section 10 */}
+            <h1>{title.toUpperCase()}</h1>
+            <p>{body}</p>
+            {/* can input expression */}
+            {/* {Math.random() + (5 + 5)} */}
+
+            
+            <div className="comments">
+                <h3>Comments ({comments.length})</h3>
+                <ul>
+                    {comments.map((comment, index) => (
+                        <li key={index}>{comment.text}</li>
+                    ))}    
+                </ul>
+            </div>
+            {/* end section 10 */}
+        </div>
+    )
 }
 
 export default App
