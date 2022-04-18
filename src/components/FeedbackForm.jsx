9e62fa4c6a0f730b4877190610f5c6dc401f4dd4
gilang aditya <madegilangaditya32@gmail.com>
2022-04-18 20:18:36 +0800

    finish section 22

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -6,8 +6,15 @@ import {useState} from 'react'
 // Section 20
 import Button from './shared/Button'
 
+// Section 22
+import RatingSelect from './RatingSelect'
+
 function FeedbackForm() {
     const [text, setText] = useState('')
+
+    // Section 22
+    const [rating, setRating] = useState(10)
+    console.log(rating)
 
     // Section 21
     const [btnDisabled, setBtnDisabled] = useState(true)
@@ -32,6 +39,10 @@ function FeedbackForm() {
         <form>
             <h2>How would you rate your service with us?</h2>
             {/* todo - rating select component */}
+            <RatingSelect select={(rating) => {
+                setRating(rating)
+                
+                }}/>
             
             <div className="input-group">
                 <input onChange={handleTextChange} type="text" placeholder='Write a Review' value={text}/>
